903523ff44b28aa95e064a9d32ec453e16150d1e
mjhea0 <hermanmu@gmail.com>
2014-04-07 16:28:53 -0600

    initial

---
+++ craigs.py
@@ -0,0 +1,20 @@
+class CraigslistAptScraper(object):
+
+    def init(self):
+        pass
+
+    def gather_info(self):
+        pass
+
+    def extract_rss_link(self):
+        pass
+
+    def collect_emails(self):
+        pass
+
+    def send_emails(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ test_craigs.py
@@ -0,0 +1,11 @@
+import unittest
+from craigs import CraigslistAptScraper
+
+
+class CraigslistAptScraperTest(unittest.TestCase):
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()

37fee23574f2c842a1c4d9c7dbde1143d19eae7e
mjhea0 <hermanmu@gmail.com>
2014-04-08 14:44:08 -0600

    updated gmail settings

--- craigs.py
+++ craigs.py
@@ -1,6 +1,8 @@
-import feedparser
-import smtplib
 import os
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+import feedparser
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,8 +18,8 @@ class CraigslistAptScraper(object):
         self.url = 'http://{}.craigslist.org/search/hhh?bedrooms={}&catAbb=hhh&maxAsk={}&minAsk={}&s=0&format=rss'.format(self.city, self.number_of_bedrooms, self.max_price, self.min_price)
         self.fromaddr = raw_input("Enter your gmail address: ")
         self.gmail_password = raw_input("Enter your gmail password: ")
-        # self.subject = 'regarding your listing on craigslist'  # update, if necessary
-        self.msg = 'Hi, I\'m looking for a place to live in the area. Would it be possible to set up a time to come by and have a look? Thanks so much!'  # update, if necessary
+        self.subject = 'regarding your listing on craigslist'  # update, if necessary
+        self.content = 'Hi, I\'m looking for a place to live in the area. Would it be possible to set up a time to come by and have a look? Thanks so much!'  # update, if necessary
 
     def extract_rss_link(self):
         os.system(['clear', 'cls'][os.name == 'nt'])
@@ -50,21 +52,26 @@ class CraigslistAptScraper(object):
             except NoSuchElementException:
                 pass
             driver.quit
+        # remove duplicate emails
         print list(set(email_addresses))
         return email_addresses
 
     def send_emails(self, all_emails):
-        # remove duplicate emails
-        print list(set(all_emails))
         all_emails = ['hermanmu@gmail.com']
         print "sending emails ..."
-        # fire up gmail
+        # connect to the server
         server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
         server.starttls()
         server.login(self.fromaddr, self.gmail_password)
         count = 1
-        for toaddrs in all_emails:
-            server.sendmail(self.fromaddr, toaddrs, self.msg)
+        for toaddr in all_emails:
+            msg = MIMEMultipart()
+            msg['From'] = self.fromaddr
+            msg['To'] = toaddr
+            msg['Subject'] = self.subject
+            msg.attach(MIMEText(self.content))
+            server.sendmail(self.fromaddr, toaddr, msg.as_string())
             print "sent {} email(s)".format(count)
             count += 1
         server.quit()
